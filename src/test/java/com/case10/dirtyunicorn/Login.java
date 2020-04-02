5ca5ff48cf4897d3c2b8031bb36b0225064a3494
Ujjaini <ujjainimondal1993@gmail.com>
2020-04-02 08:39:11 -0700

    added testNG support

--- src/test/java/com/case10/dirtyunicorn/Login.java
+++ src/test/java/com/case10/dirtyunicorn/Login.java
@@ -1,16 +1,33 @@
 package com.case10.dirtyunicorn;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-public class Login extends Init{
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class Login extends Init{
+	
+	@Test
+	public void case10login() throws IOException {
 		// TODO Auto-generated method stub
-		WebDriver driver=initChrome();
-		driver.get("https://www.case10.com/");
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		WebDriver driver=initFirefox();
+		Properties prop=new Properties();
+		FileInputStream fis=new FileInputStream("C:\\Users\\Ujjaini\\Documents\\MyProject\\dirtyunicorn\\details.properties");
+		prop.load(fis);
+		driver.get(prop.getProperty("url"));
+		String username = prop.getProperty("username");
+		String password= prop.getProperty("password");
+		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//div[@class='header-button']")).click();
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@name='login']")).click();
+		
 		
 	}
 	
